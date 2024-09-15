d44a765a725e4cfb277fbda2784d8e78caa6d23c
Leon-JulianHoneycutt <L_Julianhoneycutt@insideranken.org>
2024-09-15 17:21:16 -0400

    save

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import debug from 'debug';
+const debugIndex = debug('app:index');
+
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  debugIndex(`Example app listening on port http://localhost:${port}`)
+});
